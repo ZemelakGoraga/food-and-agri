a9b93c186d73ba4df5f62649f25c61f048274c1c
zemelak-s-goraga <zgoraga@my365.bellevue.edu>
2023-09-07 02:17:30 -0400

    git commit -m "#Change#:1 #Change(s) Made: Added date and time handling to show current date and time on the receipt input lines 31-37 added #Date of Change: 9/7/2023 #Author: Zemelak Goraga #Change Approved by: self #Date Moved to Production: 9/7/2023 "

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+# The following codes may help you to do your assignment on Python programing:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Send a welcome message to the new team member in DSC 510 program
+
+print("Welcome everyone to the DSC 510!")
+
+# Retrieve first name of the team members
+first_name = input("May I know your first name please?: ")
+
+# Retrieve from the team members the number of books they purchased in this semester.
+
+number_of_books = int(input("Enter the number of books you bought in this semester: "))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Calculate the total cost of buying books for each member
+cost_per_book = 20
+total_cost = number_of_books * cost_per_book
 
+# Print a receipt for the user
+print("\nReceipt for", first_name)
+print("Number of books bought by the team member:", number_of_books)
+print("Total cost of buying book: $", total_cost)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#Change#:1
+#Change(s) Made: Added date and time handling to show current date and time on the receipt input lines 31-37 added
+#Date of Change: 9/7/2023
+#Author: zd ghj
+#Change Approved by: sddff ghhhh
+#Date Moved to Production: 9/7/2023
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import datetime
+# Get the current date and time
+current_date_time = datetime.datetime.now()
+# Format the date and time as a string
+formatted_date_time = current_date_time.strftime("%Y-%m-%d %H:%M:%S")
+# Print the formatted date and time
+print("Current date and time:", formatted_date_time)
